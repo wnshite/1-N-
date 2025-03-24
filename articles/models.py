8d4d0c64f7f626628663d89c655b8d75da7a86e4
seunghyun <tmdgus_skfk@naver.com>
2025-03-24 12:13:42 +0900

    comment 댓글 생성

--- articles/models.py
+++ articles/models.py
@@ -7,5 +7,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-# class Comment(models.Model):
-#   passs
+class Comment(models.Model):  # 댓글 생성
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE) # 위의 article모델과 comment모델이 동일함
+    # 게시물을 지웠으면 그 게시물에 존재했던 댓글은 당연히 지워진다. -> 이 개념이 CASCADE
